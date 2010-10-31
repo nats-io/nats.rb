01376386258dbaad4ca54abb65007274921eade3
Derek Collison <derek.collison@gmail.com>
2010-10-31 15:35:03 -0500

    Moved option parsing

---
+++ lib/nats/server/options.rb
@@ -0,0 +1,86 @@
+
+require 'optparse'
+require 'yaml'
+
+module NATS
+  class Server
+
+    class << self
+      def parser
+        @parser ||= OptionParser.new do |opts|
+          opts.banner = "Usage: nats [options]"
+
+          opts.separator ""
+          opts.separator "Server options:"
+
+          opts.on("-a", "--addr HOST", "bind to HOST address " +
+                                       "(default: #{@options[:addr]})")                 { |host| @options[:address] = host }
+          opts.on("-p", "--port PORT", "use PORT (default: #{@options[:port]})")        { |port| @options[:port] = port.to_i }
+        
+          opts.on("-d", "--daemonize", "Run daemonized in the background")              { @options[:daemonize] = true }
+          opts.on("-l", "--log FILE", "File to redirect output " +                      
+                                      "(default: #{@options[:log_file]})")              { |file| @options[:log_file] = file }
+          opts.on("-T", "--logtime", "Timestamp log entries")                           { @options[:log_time] = true }
+
+          opts.on("-P", "--pid FILE", "File to store PID " +                            
+                                      "(default: #{@options[:pid_file]})")              { |file| @options[:pid_file] = file }
+
+          opts.on("-C", "--config FILE", "Configuration File " +                            
+                                      "(default: #{@options[:config_file]})")           { |file| @options[:config_file] = file }
+
+          opts.separator ""
+          opts.separator "Authorization options: (Should be done in config file for production)"
+
+          opts.on("--user user", "User required for connections")                       { |user| @options[:user] = user }
+
+          opts.on("--pass password", "Password required for connections")               { |pass| @options[:pass] = pass }
+          opts.on("--password password", "Password required for connections")           { |pass| @options[:pass] = pass }
+
+          opts.separator ""
+          opts.separator "Common options:"
+
+          opts.on_tail("-h", "--help", "Show this message")                             { puts opts; exit }
+          opts.on_tail('-v', '--version', "Show version")                               { puts NATS::Server.version; exit }
+          opts.on_tail("-D", "--debug", "Set debugging on")                             { @options[:debug] = true }
+          opts.on_tail("-V", "--trace", "Set tracing on of raw protocol")               { @options[:trace] = true }
+        end
+      end
+
+      def read_config_file
+        return unless config_file = @options[:config_file]
+        config = File.open(config_file) { |f| YAML.load(f) }
+        # Command lines args, parsed first, will override these.
+        [:addr, :port, :log_file, :pid_file, :user, :pass, :log_time, :debug].each do |p|
+          c = config[p.to_s]
+          @options[p] = c if c and not @options[p]
+        end
+        rescue => e
+          log "Could not read configuration file:  #{e}"
+          exit
+      end
+
+
+      def finalize_options
+        # Addr/Port
+        @options[:port] ||= DEFAULT_PORT
+        @options[:addr] ||= '0.0.0.0'
+
+        # Debug and Tracing
+        @debug_flag = @options[:debug]
+        @trace_flag = @options[:trace]
+        debug @options # Block pass?
+        debug "DEBUG is on"
+        trace "TRACE is on"
+        
+        # Auth
+        @auth_required = (@options[:user] != nil)
+
+        # Redirect for logs if needed, and timestamp option
+        @log_time = @options[:log_time]
+        $stdout = File.new(@options[:log_file], 'w') if @options[:log_file]
+      end
+
+    end
+
+  end
+end
