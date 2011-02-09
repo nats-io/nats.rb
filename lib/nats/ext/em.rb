ed5b1a21ab623cbeb427d7652983d9d26052c0f2
Derek Collison <derek.collison@gmail.com>
2011-02-08 21:41:40 -0600

    Check for method on instance

--- lib/nats/ext/em.rb
+++ lib/nats/ext/em.rb
@@ -6,7 +6,7 @@ rescue LoadError
 end
 
 # Check for get_outbound_data_size support, fake it out if it doesn't exist, e.g. jruby
-if !EM::Connection.respond_to? :get_outbound_data_size
+if !EM::Connection.method_defined? :get_outbound_data_size
   class EM::Connection
     def get_outbound_data_size; return 0; end
   end
