805c1f8c09ce35489214edf7f8109cf4dc1d3c7f
Derek Collison <derek.collison@gmail.com>
2011-12-20 20:19:38 -0600

    Removed additional args

--- spec/ssl_spec.rb
+++ spec/ssl_spec.rb
@@ -4,15 +4,13 @@ require 'fileutils'
 describe 'ssl' do
 
   before (:all) do
-
     TEST_SERVER_SSL = "nats://localhost:9392"
     TEST_SERVER_SSL_PID = '/tmp/nats_ssl.pid'
-    TEST_SERVER_SSL_LOG = '/tmp/nats_ssl.log'
 
     TEST_SERVER_NO_SSL = "nats://localhost:9394"
     TEST_SERVER_NO_SSL_PID = '/tmp/nats_no_ssl.pid'
 
-    @s_ssl = NatsServerControl.new(TEST_SERVER_SSL, TEST_SERVER_SSL_PID, "--ssl -VD -l #{TEST_SERVER_SSL_LOG}")
+    @s_ssl = NatsServerControl.new(TEST_SERVER_SSL, TEST_SERVER_SSL_PID, "--ssl")
     @s_ssl.start_server
 
     @s_no_ssl = NatsServerControl.new(TEST_SERVER_NO_SSL, TEST_SERVER_NO_SSL_PID)
@@ -23,11 +21,10 @@ describe 'ssl' do
     @s_ssl.kill_server
     @s_no_ssl.kill_server
     FileUtils.rm_f TEST_SERVER_SSL_PID
-    FileUtils.rm_f TEST_SERVER_SSL_LOG
     FileUtils.rm_f TEST_SERVER_NO_SSL_PID
   end
 
-  it 'should fail to connect to an ssl server without tls/ssl negotiation' do
+  it 'should fail to connect to an ssl server without TLS/SSL negotiation' do
     expect do
       NATS.start(:uri => TEST_SERVER_SSL) { NATS.stop }
     end.to raise_error NATS::Error
