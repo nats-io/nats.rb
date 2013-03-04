cd9207bf61331a408354f9e6c80097462b2cef42
Derek Collison <derek.collison@gmail.com>
2013-03-03 17:35:56 -0800

    Bumped version

--- lib/nats/client.rb
+++ lib/nats/client.rb
@@ -8,7 +8,7 @@ require "#{ep}/ext/json"
 
 module NATS
 
-  VERSION = "0.5.0.beta.4".freeze
+  VERSION = "0.5.0.beta.6".freeze
 
   DEFAULT_PORT = 4222
   DEFAULT_URI = "nats://localhost:#{DEFAULT_PORT}".freeze
--- lib/nats/server/const.rb
+++ lib/nats/server/const.rb
@@ -1,7 +1,7 @@
 
 module NATSD #:nodoc:
 
-  VERSION  = '0.5.0.beta.4'
+  VERSION  = '0.5.0.beta.6'
   APP_NAME = 'nats-server'
 
   DEFAULT_PORT = 4222
