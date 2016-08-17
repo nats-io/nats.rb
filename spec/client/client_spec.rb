2cb5839318417a30d53664e8d14c75bd0b4dede6
Waldemar Quevedo <wally@apcera.com>
2016-08-16 19:53:38 -0700

    Use current client version in connect string from test

--- spec/client/client_spec.rb
+++ spec/client/client_spec.rb
@@ -393,7 +393,7 @@ describe 'Client - specification' do
 
   it 'should allowing setting name for the client on connect' do
     with_em_timeout do
-      connect_command = "CONNECT {\"verbose\":false,\"pedantic\":false,\"lang\":\"ruby\",\"version\":\"0.8.0\",\"protocol\":1,\"name\":\"hello\"}\r\n"
+      connect_command = "CONNECT {\"verbose\":false,\"pedantic\":false,\"lang\":\"ruby\",\"version\":\"#{NATS::VERSION}\",\"protocol\":1,\"name\":\"hello\"}\r\n"
       conn = NATS.connect(:name => "hello")
       expect(conn.connect_command).to eql(connect_command)
     end
