564fbc4bf60d22b79b93ac9803be3a7db4bf4ee7
Waldemar Quevedo <wally@apcera.com>
2017-03-02 16:44:34 -0800

    Use RUBY_ENGINE when announcing runtime on CONNECT

--- lib/nats/version.rb
+++ lib/nats/version.rb
@@ -1,6 +1,6 @@
 module NATS
   # NOTE: These are all announced to the server on CONNECT
   VERSION = "0.8.2".freeze
-  LANG    = (RUBY_PLATFORM == 'java' ? 'jruby' : 'ruby').freeze
+  LANG    = RUBY_ENGINE
   PROTOCOL_VERSION = 1
 end
