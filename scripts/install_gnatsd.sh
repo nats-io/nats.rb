8fb166f8ffe9d3b3c981a8eebe8c25836636d864
Waldemar Quevedo <wally@synadia.com>
2018-02-11 15:51:56 -0800

    Update NATS server version

--- scripts/install_gnatsd.sh
+++ scripts/install_gnatsd.sh
@@ -5,9 +5,9 @@ set -e
 if [ ! "$(ls -A $HOME/gnatsd)" ]; then
   mkdir -p $HOME/gnatsd
   cd $HOME/gnatsd
-  wget https://github.com/nats-io/gnatsd/releases/download/v0.9.4/gnatsd-v0.9.4-linux-amd64.zip -O gnatsd.zip
+  wget https://github.com/nats-io/gnatsd/releases/download/v1.0.4/gnatsd-v1.0.4-linux-amd64.zip -O gnatsd.zip
   unzip gnatsd.zip
-  mv gnatsd-v0.9.4-linux-amd64/gnatsd .
+  mv gnatsd-v1.0.4-linux-amd64/gnatsd .
 else
   echo 'Using cached directory.';
 fi
